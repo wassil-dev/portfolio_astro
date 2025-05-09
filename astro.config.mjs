44c2417bfa8987f8ae11e5434c8ff1a0a8ca2903
wassil <ouassil.dahimene@gmail.com>
2025-05-09 23:29:05 +0100

    aazez

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/',
+  site: 'https://ouassil-dahimene.dev', // Your custom domain
+  output: 'static',
   integrations: [tailwind()],
 });
