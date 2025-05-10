f3cf5d3b9dda242bd7f3beb001c95d19bfd9e1b5
wassil <ouassil.dahimene@gmail.com>
2025-05-10 12:16:18 +0100

    update info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from 'astro-sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ouassil-dahimene.dev',
+  integrations: [sitemap()],
   output: 'static',
   integrations: [tailwind()],
 });
